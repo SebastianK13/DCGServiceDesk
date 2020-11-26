82f44ce7d82a8f3a3aad72593e61c6e3ea5ff69e
sebastianknych95@gmail.com <sebastianknych95@gmail.com>
2020-11-26 21:42:12 +0100

    Entity framework core and dependency injection implementation inprogress.

---
+++ DCGServiceDesk.Authorization/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DCGServiceDesk.Authorization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- DCGServiceDesk/Models/AppIdentityDbContext.cs
+++ DCGServiceDesk/Models/AppIdentityDbContext.cs
@@ -1,15 +1,25 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using DarkClusterTechnologyEnterprise.Models;
+using System.Threading.Tasks;
 using System;
-using System.Collections.Generic;
-using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace DCGServiceDesk.Models
 {
-    public class DataContext : DbContext
+    public class AppIdentityDbContext : DbContext
     {
+        public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options)
+            : base(options) { }
 
-        public DataContext(DbContextOptions<DataContext> options) : base(options)
-        {
-        }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Earnings> Earnings { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Presence> Presences { get; set; }
+        public DbSet<TaskSchedule> TaskSchedules { get; set; }
+        public DbSet<Break> Breaks { get; set; }
+        public DbSet<TimeZonesModel> Zone { get; set; }
+        public DbSet<Position> Positions { get; set; }
     }
 }
